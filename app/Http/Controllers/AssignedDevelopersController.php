06cf2b5ea79062688dfa95a88f239841415288d0
unknown <saadmansakib275@gmail.com>
2024-04-27 11:35:09 +0600

    models created

---
+++ app/Http/Controllers/AssignedDevelopersController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assigned_developers;
+use App\Http\Requests\StoreAssigned_developersRequest;
+use App\Http\Requests\UpdateAssigned_developersRequest;
+
+class AssignedDevelopersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAssigned_developersRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Assigned_developers $assigned_developers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Assigned_developers $assigned_developers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAssigned_developersRequest $request, Assigned_developers $assigned_developers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Assigned_developers $assigned_developers)
+    {
+        //
+    }
+}
